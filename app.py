d12ea451d6ac3096b70e431d91c45fb4063a5ed0
bamjun <khy5116@naver.com>
2024-09-01 14:01:57 +0900

    say hi

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ import os
 import discord
 from discord.ext import commands
 
-# 필요한 권한을 설정
+# 봇 인스턴스 생성
 intents = discord.Intents.default()
-intents.message_content = True  # 메시지 콘텐츠를 읽는 권한을 설정
+intents.message_content = True  # 메시지 콘텐츠 인텐트 활성화
 
-# 봇 객체를 생성할 때 intents를 추가
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'We have logged in as {bot.user}')
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send("Hello, world!")
+@bot.event
+async def on_message(message):
+    # 봇 자신의 메시지는 무시
+    if message.author == bot.user:
+        return
+    
+    # 사용자가 "hi"라고 입력하면 "hello"로 응답
+    if message.content.lower() == 'hi':
+        await message.channel.send('hello')
+    
+    # 다른 명령어도 처리할 수 있도록 on_message에서 명령어 처리기를 호출
+    await bot.process_commands(message)
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
