9438f6d534888954cab0db684f53751abc3ff18c
bamjun <khy5116@naver.com>
2024-09-01 13:13:34 +0900

    tes script[D[D[D[D[D[D[Dt

---
+++ discord.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello, world!")
+
+bot.run('YOUR_DISCORD_BOT_TOKEN')
